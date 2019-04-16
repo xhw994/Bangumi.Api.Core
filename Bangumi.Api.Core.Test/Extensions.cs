d90a3615d36422c4ff344c60d8ee051a12cea9d8
xwa132 <xwa132@sfu.ca>
2019-04-16 04:06:16 -0700

    replace path token

---
+++ Bangumi.Api.Core.Test/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bangumi.Api.Core.Extensions;
+
+namespace Bangumi.Api.Core.Test
+{
+    [TestClass]
+    public class Extensions
+    {
+        [TestMethod]
+        public void ReplacePathVariables()
+        {
+            string path = @"/collection/{subject_id}/{action}";
+            string[] vars = { "9527", "wish" };
+            string[] vars_longer = { "9527", "wish", "should_not_exist" };
+            string[] vars_shorter = { "9527" };
+
+            Assert.AreEqual(string.Empty, string.Empty.ReplacePathVariables(vars));
+            Assert.AreEqual(@"/collection/9527/wish", path.ReplacePathVariables(vars));
+            Assert.AreEqual(@"/collection/9527/wish", path.ReplacePathVariables(vars_longer));
+            Assert.AreEqual(@"/collection/9527/{action}", path.ReplacePathVariables(vars_shorter));
+        }
+    }
+}
---
+++ src/Extensions/StringExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Bangumi.Api.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplacePathVariables(this string path, params string[] vars)
+        {
+            if (vars.Length == 0) return path;
+            if (string.IsNullOrEmpty(path)) return string.Empty;
+
+            const char sep = (char)47;
+            string[] parts = path.Split(new char[] { sep }, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0, vc = 0; i < parts.Length; i++)
+            {
+                if (parts[i][0] == '{')
+                {
+                    parts[i] = ParameterToString(vars[vc++]);
+                    if (vc == vars.Length) break;
+                }
+            }
+
+            return sep + string.Join(sep.ToString(), parts);
+        }
+
+        /// <summary>
+        /// If parameter is DateTime, output in a formatted string (default ISO 8601), customizable with Configuration.DateTime.
+        /// If parameter is a list of string, join the list with ",".
+        /// Otherwise just return the string.
+        /// </summary>
+        /// <param name="obj">The parameter (header, path, query, form).</param>
+        /// <returns>Formatted string.</returns>
+        public static string ParameterToString(object obj)
+        {
+            switch (obj)
+            {
+                // Deal with this later
+                case DateTime _:
+                    // Return a formatted date string - Can be customized with Configuration.DateTimeFormat
+                    // Defaults to an ISO 8601, using the known as a Round-trip date/time pattern ("o")
+                    // https://msdn.microsoft.com/en-us/library/az4se3k1(v=vs.110).aspx#Anchor_8
+                    // For example: 2009-06-15T13:45:30.0000000
+                    return ((DateTime)obj).ToString("o");
+                case List<string> _:
+                    return string.Join(",", (obj as List<string>).ToArray());
+                default:
+                    return Convert.ToString(obj);
+            }
+        }
+    }
+}
