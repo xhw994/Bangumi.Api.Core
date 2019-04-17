c2e6b6e699d8ed03555e617b2bd090f409d9a48f
xwa132 <xwa132@sfu.ca>
2019-04-16 20:51:34 -0700

    resgroup

---
+++ src/Model/Definition/ResponseGroup.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace Bangumi.Api.Core.Model.Definition
+{
+
+    /// <summary>
+    /// 返回数据大小
+    /// </summary>
+    [DataContract]
+    public enum ResponseGroup
+    {
+        [Description("small")]
+        Small,
+        [Description("medium")]
+        Medium,
+        [Description("large")]
+        Large
+    }
+}
