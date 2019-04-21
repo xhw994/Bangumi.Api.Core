4911b8f6c65ad2c040d8075d5a520c289594b170
xwa132 <xwa132@sfu.ca>
2019-04-21 03:47:40 -0700

    Add token models

---
+++ src/Model/Token/RefreshTokenResponse.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Bangumi.Api.Core.Model.Token
+{
+    /// <summary>
+    /// 授权有效期刷新
+    /// </summary>
+    [DataContract]
+    public class RefreshTokenResponse
+    {
+        /// <summary>
+        /// Gets or Sets AccessToken
+        /// </summary>
+        [DataMember(Name = "access_token", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "access_token")]
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or Sets ExpiresIn
+        /// </summary>
+        [DataMember(Name = "expires_in", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "expires_in")]
+        public int? ExpiresIn { get; set; }
+
+        /// <summary>
+        /// Gets or Sets TokenType, this should be 'Bearer'
+        /// </summary>
+        [DataMember(Name = "token_type", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "token_type")]
+        public string TokenType { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Scope, this should be null
+        /// </summary>
+        [DataMember(Name = "scope", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "scope")]
+        public string Scope { get; set; }
+
+        /// <summary>
+        /// Gets or Sets RefreshToken
+        /// </summary>
+        [DataMember(Name = "refresh_token", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "refresh_token")]
+        public string RefreshToken { get; set; }
+
+
+        /// <summary>
+        /// Get the string presentation of the object
+        /// </summary>
+        /// <returns>string presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class Images {\n");
+            sb.Append("  AccessToken: ").Append(AccessToken).Append("\n");
+            sb.Append("  ExpiresIn: ").Append(ExpiresIn).Append("\n");
+            sb.Append("  TokenType: ").Append(TokenType).Append("\n");
+            sb.Append("  Scope: ").Append("null").Append("\n");
+            sb.Append("  RefreshToken: ").Append(RefreshToken).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
