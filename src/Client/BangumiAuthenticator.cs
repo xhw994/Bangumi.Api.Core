b7e78c584e61d5072d4a6aecad1638204a9da0f8
xwa132 <xwa132@sfu.ca>
2019-04-24 02:33:38 -0700

    finish authenticatior

--- src/Client/BangumiAuthenticator.cs
+++ src/Client/BangumiAuthenticator.cs
@@ -7,68 +7,45 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using static Bangumi.Api.Core.Extension.StringExtension;
+using static Bangumi.Api.Core.Configuration;
+using Bangumi.Api.Core.Model.Token;
+using Newtonsoft.Json;
 
 namespace Bangumi.Api.Core.Client
 {
     public class BangumiAuthenticator : IAuthenticator
     {
-        private readonly string codePath = "https://bgm.tv/oauth/authorize";
-        private readonly string tokenPath = "https://bgm.tv/oauth/access_token";
-
-        private readonly string _appId;
-        private readonly string _appSecret;
+        // TODO: Implement state param
 
         private string _authHeader;
-
         private bool _authorized;
 
-        public BangumiAuthenticator(string appId, string appSecret, string domainName = null, string ipv4Adress = null, string route = null, int port = 5994)
+        public BangumiAuthenticator()
         {
             // Value check, future need to check length and domain properties
-            if (!IsAlphaNumeric(appId) || !appId.StartsWith("bgm"))
+            if (!IsAlphaNumeric(AppId) || !AppId.StartsWith("bgm"))
             {
-                throw new ArgumentException($"Invalid application ID <{appId}>. Application IDs should start with `bgm` followed by alphanumeric values");
+                throw new ArgumentException($"Invalid application ID <{AppId}>. Application IDs should start with `bgm` followed by alphanumeric values");
             }
-            if (!IsAlphaNumeric(appSecret))
+            if (!IsAlphaNumeric(AppSecret))
             {
-                throw new ArgumentException($"Invalid application secret. Application secrets should only contain alphanumeric values.");
+                throw new ArgumentException($"Invalid application secret <{AppSecret}>. Application secrets should only contain alphanumeric values.");
             }
-
-            // Set credentials
-            _appId = appId;
-            _appSecret = appSecret;
-
-            // Set domain and ipv4 properties
-            DomainName = domainName;
-            Ipv4Address = ipv4Adress;
-            Route = route;
-            Port = port;
         }
-
-        public string DomainName { get; set; }
-        public string Ipv4Address { get; set; }
-        public string Route { get; set; }
-        public int Port { get; set; } = 5994;
 
         #region Authorization code
 
         private void RequestAuthCode()
         {
-            CallbackListner listner = new CallbackListner(Port)
-            {
-                DomainName = DomainName,
-                Ipv4Address = Ipv4Address,
-                Route = Route,
-                Port = Port
-            }.AddPrefixes();
+            CallbackListner listner = new CallbackListner(CallbackUrl);
 
-            string codeUrl = $"{codePath}?client_id={_appId}&response_type=code";
+            string codeUrl = $"{AuthCodeUrl}?client_id={AppId}&response_type=code";
             OpenBrowser(codeUrl);
-            AuthCode = listner.GetCode();
             authcodeTime = DateTime.Now;
+            AuthCode = listner.GetCode();
         }
 
-        public string AuthCode { get; set; }
+        public string AuthCode { get; private set; }
         public bool AuthCodeExpired { get => !string.IsNullOrEmpty(AuthCode) && authcodeTime + TimeSpan.FromMinutes(1) > DateTime.Now; }
         private DateTime authcodeTime;
 
@@ -76,59 +53,106 @@ namespace Bangumi.Api.Core.Client
 
         #region Access Token
 
-        public string AccessToken { get; set; }
-        public string RefreshToken { get; set; }
+        public string AccessToken { get; private set; }
+        public string RefreshToken { get; private set; }
+        public bool TokenExpired { get => !string.IsNullOrEmpty(AccessToken) && tokenExpireTime > DateTime.Now; }
+        public bool Authenticated { get => TokenExpired; }
+        private DateTime tokenExpireTime;
 
         private void RequestAccessToken(IRestClient client)
         {
-            if (!string.IsNullOrEmpty(AccessToken))
-            {
-                RequestTokenRefresh(client);
-            }
-
             // Compose the post request
-            string tokenUrl = "https://bgm.tv/oauth/access_token";
             Dictionary<string, string> queryParams = new Dictionary<string, string>()
             {
                 { "grant_type", "authorization_code" },
-                { "client_id", _appId },
-                { "client_secret", _appSecret },
+                { "client_id", AppId },
+                { "client_secret", AppSecret },
                 { "code", AuthCode },
-                { "redirect_uri", "" }
+                { "redirect_uri", CallbackUrl }
             };
+            RestRequest request = ComposePostRequest(TokenUrl, queryParams);
+
+            // Get the response
+            DateTime now = DateTime.Now;
+            IRestResponse response = client.Execute(request);
+            if ((int)response.StatusCode >= 400)
+            {
+                throw new ApiException((int)response.StatusCode, $"There is an error when requesting access token: " + response.Content, response.Content);
+            }
+            else if (response.StatusCode == 0)
+            {
+                throw new ApiException((int)response.StatusCode, $"There is an error when requesting access token: " + response.ErrorMessage, response.ErrorMessage);
+            }
+            
+            // Deserialize response to token and update fields
+            GetTokenResponse tokenResponse = (GetTokenResponse)JsonConvert.DeserializeObject(response.Content, typeof(GetTokenResponse));
+            if (string.IsNullOrEmpty(tokenResponse.AccessToken) || string.IsNullOrEmpty(tokenResponse.RefreshToken) || !tokenResponse.ExpiresIn.HasValue)
+            {
+                throw new ApiException(400, "Invalid response from server: " + tokenResponse.ToString());
+            }
+            AccessToken = tokenResponse.AccessToken;
+            RefreshToken = tokenResponse.RefreshToken;
+            tokenExpireTime = now + TimeSpan.FromSeconds(tokenResponse.ExpiresIn.Value - 60); // Reduce 1 min for possible network issues.
         }
 
         private void RequestTokenRefresh(IRestClient client)
         {
+            // Compose the post request
+            Dictionary<string, string> queryParams = new Dictionary<string, string>()
+            {
+                { "grant_type", "authorization_code" },
+                { "client_id", AppId },
+                { "client_secret", AppSecret },
+                { "refresh_token", RefreshToken },
+                { "redirect_uri", CallbackUrl }
+            };
+            RestRequest request = ComposePostRequest(TokenUrl, queryParams);
 
+            // Get the response
+            DateTime now = DateTime.Now;
+            IRestResponse response = client.Execute(request);
+            if ((int)response.StatusCode >= 400)
+            {
+                throw new ApiException((int)response.StatusCode, $"There is an error when requesting token refresh: " + response.Content, response.Content);
+            }
+            else if (response.StatusCode == 0)
+            {
+                throw new ApiException((int)response.StatusCode, $"There is an error when requesting token refresh: " + response.ErrorMessage, response.ErrorMessage);
+            }
+
+            // Deserialize response to token and update fields
+            RefreshTokenResponse tokenResponse = (RefreshTokenResponse)JsonConvert.DeserializeObject(response.Content, typeof(RefreshTokenResponse));
+            if (string.IsNullOrEmpty(tokenResponse.AccessToken) || string.IsNullOrEmpty(tokenResponse.RefreshToken) || !tokenResponse.ExpiresIn.HasValue)
+            {
+                throw new ApiException(400, "Invalid response from server: " + tokenResponse.ToString());
+            }
+            AccessToken = tokenResponse.AccessToken;
+            RefreshToken = tokenResponse.RefreshToken;
+            tokenExpireTime = now + TimeSpan.FromSeconds(tokenResponse.ExpiresIn.Value - 60); // Reduce 1 min for possible network issues.
         }
 
         #endregion
 
-
-
-
-
-
-
-
         public void Authenticate(IRestClient client, IRestRequest request)
         {
-            if (string.IsNullOrEmpty(AccessToken))
+            // Request Auth Token if not yet authenticated
+            if (TokenExpired)
             {
-                while (!AuthCodeExpired)
+                // If AuthCode is expired, request AuthCode first
+                if (AuthCodeExpired)
                 {
-                    // Request authorization code with app_id.
                     RequestAuthCode();
                 }
+                RequestAccessToken(client);
+            }
+            // Refresh token when it is only valid for no more than 5 minutes
+            else if (DateTime.Now + TimeSpan.FromMinutes(5) > tokenExpireTime)
+            {
+                RequestTokenRefresh(client);
             }
 
-
-
-            // only add the Authorization parameter if it hasn't been added by a previous Execute
-            if (!request.Parameters.Any(p => p.Type.Equals(ParameterType.HttpHeader) &&
-                                             p.Name.Equals("Authorization", StringComparison.OrdinalIgnoreCase)))
-                request.AddParameter("Authorization", _authHeader, ParameterType.HttpHeader);
+            // If everything is set, add `Authorization: Bear <Token>` to header
+            request.AddHeader("Authorization", $"Bearer {AccessToken}");
         }
 
 
@@ -168,6 +192,17 @@ namespace Bangumi.Api.Core.Client
                     throw;
                 }
             }
+        }
+
+        private RestRequest ComposePostRequest(string url, Dictionary<string, string> queryParams)
+        {
+            RestRequest request = new RestRequest(url, Method.POST);
+            request.AddHeader("Accept", "application/json");
+            foreach (var param in queryParams)
+            {
+                request.AddParameter(param.Key, param.Value, ParameterType.GetOrPost);
+            }
+            return request;
         }
 
         #endregion
