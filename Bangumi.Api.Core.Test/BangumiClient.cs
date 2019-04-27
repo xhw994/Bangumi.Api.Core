1f9b3181bebc84d4a0d12a3dd8ac2ce645fedb6f
xwa132 <xwa132@sfu.ca>
2019-04-27 01:05:37 -0700

    simple client test

---
+++ Bangumi.Api.Core.Test/BangumiClient.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Bangumi.Api.Core.Client;
+
+namespace Bangumi.Api.Core.Test
+{
+    [TestClass]
+    public class BangumiClient
+    {
+        [TestMethod]
+        public void GetCodeFromListener()
+        {
+            //string callbackUrl = "http://174.1.60.140:5994/";
+            string callbackUrl = "http://localhost:5994/";
+            CallbackListner listner = new CallbackListner(callbackUrl);
+
+            string code = listner.GetCode();
+            Assert.IsFalse(string.IsNullOrEmpty(code), "Listener returned empty code");
+            Console.WriteLine(code);
+        }
+    }
+}
